93660ad97f17f12dd9c6cfa69e3ff7eddc42be40
am.ghorbani <am.ghorbani@alibaba.ir>
2023-02-09 14:44:44 +0330

    Add an example for alternative classes with different interfaces

---
+++ src/ObjectOrientedAbusers/AlternativeClassesWithDifferentInterfaces/Before/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjectOrientedAbusers.AlternativeClassesWithDifferentInterfaces.Before;
+
+public static class StringExtensions
+{
+    public static string NormalizeContent(this string content)
+    {
+        content = content.Replace("\"", "'");
+        content = content.Replace("{", "[");
+        content = content.Replace("}", "]");
+
+        return content;
+    }
+}
---
+++ src/ObjectOrientedAbusers/AlternativeClassesWithDifferentInterfaces/Before/StringHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjectOrientedAbusers.AlternativeClassesWithDifferentInterfaces.Before;
+
+public class StringHelper
+{
+    public static string NormalizeCharacters(string content)
+    {
+        content = content.Replace("\"", "'");
+        content = content.Replace("{", "[");
+        content = content.Replace("}", "]");
+
+        return content;
+    }
+}
